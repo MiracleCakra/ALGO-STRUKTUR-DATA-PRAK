8dad195b0eb570855a96d43953dae4778a1ec3bf
Cakra Wangsa May Ahmad Widodo <Cakra Wangsa May Ahmad Widodo>
2024-06-03 14:00:10 +0700

    UPDATE!

---
+++ minggu15/LaporanP15/Laporan15.md
@@ -0,0 +1,19 @@
+# JOBSHEET XV GRAPH
+
+Nama    : Cakra Wangsa M.A.W
+
+Kelas   : TI_1H
+
+Absen   : 07
+
+NIM     : 2341720032
+
+### 1 Tujuan Praktikum
+
+Setelah melakukan praktikum ini, mahasiswa mampu:
+
+1. memahami model graph
+2. membuat dan mendeklarasikan struktur algoritma graph
+3. menerapkan algoritma dasar graph dalam beberapa studi kasus
+
+
---
+++ minggu15/prak2/GraphMain07.java
@@ -0,0 +1,17 @@
+package minggu15.prak2;
+
+public class GraphMain07 {
+    public static void main(String[] args) {
+        GraphMatriks07 gdg = new GraphMatriks07(4);
+        gdg.makeEdge(0, 1, 50);
+        gdg.makeEdge(1, 0, 60);
+        gdg.makeEdge(1, 2, 70);
+        gdg.makeEdge(2, 1, 80);
+        gdg.makeEdge(2, 3, 40);
+        gdg.makeEdge(3, 0, 90);
+        gdg.printGraph();
+        System.out.println("Hasil setelah penghapusan edge");
+        gdg.removeEdge(2, 1);
+        gdg.printGraph();
+    }
+}
